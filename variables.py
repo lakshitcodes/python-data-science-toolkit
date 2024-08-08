9cf6630cf847ef30439211af0998fcc3782f4b68
Lakshit Jain <jainlakshit849@gmail.com>
2024-08-08 21:31:42 +0530

    Variables in Python

---
+++ variables.py
@@ -0,0 +1,35 @@
+# A variable is a container for a value, which can be of various types
+
+''' 
+This is a 
+multiline comment
+or docstring (used to define a functions purpose)
+can be single or double quotes
+'''
+
+""" 
+VARIABLE RULES : 
+ - Variable names are case sensitive (name and NAME are different variables)
+ - Must start with a letter or an underscore
+ - Can have numbers but cannot start with one
+"""
+
+# x=1             # int
+# y=2.5           #float
+# name = 'John'   #string
+# is_cool = True  #boolean
+
+ #Multiple Assignment
+x,y,name,is_cool = (1,2.5,'John',True)
+
+#Basic Math
+a = x+y
+
+#Casting
+x=str(y)
+y=int(y)
+z=float(y)
+
+
+
+ 
